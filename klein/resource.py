0e9d6fa8de9159a8651a7d16e0883b10a3a518b1
Aaron Gallagher <habnabit@gmail.com>
2011-03-07 18:30:58 -0800

    Adding route inheritance and deferred pages.

--- klein/resource.py
+++ klein/resource.py
@@ -1,11 +1,26 @@
 from twisted.web.resource import Resource
+from twisted.web import server
+from twisted.internet import defer
 from werkzeug import routing
 
 class KleinResource(object, Resource):
     class __metaclass__(type):
         def __init__(self, name, bases, attrs):
-            routes = self.map = routing.Map()
-            endpoints = self.endpoints = {}
+            inherited_rules = []
+            inherited_endpoints = {}
+            for base in reversed(bases):
+                p = getattr(base, '__klein_params__', None)
+                if p is None:
+                    continue
+
+                mapper, endpoints = p
+                inherited_rules.extend(
+                    rule.empty() for rule in mapper.iter_rules())
+                inherited_endpoints.update(endpoints)
+
+            routes = self.map = routing.Map(inherited_rules)
+            endpoints = self.endpoints = inherited_endpoints
+            self.__klein_params__ = routes, endpoints
             for value in attrs.itervalues():
                 akw = getattr(value, '__klein_exposed__', None)
                 if akw is None:
@@ -31,10 +46,20 @@ class KleinResource(object, Resource):
         if request.postpath:
             path_info = '/' + '/'.join(request.postpath)
         url_scheme = 'https' if request.isSecure() else 'http'
+
         mapper = self.map.bind(server_name, script_name, path_info=path_info,
             default_method=request.method, url_scheme=url_scheme)
         request.mapper = mapper
         request.url_for = mapper.build
         endpoint, kwargs = mapper.match()
+
         meth = self.endpoints[endpoint]
-        return meth(self, request, **kwargs)
+        d = defer.maybeDeferred(meth, self, request, **kwargs)
+        def process(r):
+            if r is not None:
+                request.write(r)
+                request.finish()
+            return r
+        d.addCallback(process)
+        d.addErrback(request.processingFailed)
+        return server.NOT_DONE_YET

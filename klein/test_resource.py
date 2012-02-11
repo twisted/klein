a0184f50415f431b88d5e8c850a1db7992bcf575
David Reid <dreid@dreid.org>
2012-02-10 20:41:52 -0800

    Support returning a twisted.web.template.Element

--- klein/test_resource.py
+++ klein/test_resource.py
@@ -6,6 +6,8 @@ from klein.resource import KleinResource
 from twisted.internet.defer import succeed, Deferred
 from twisted.web import server
 
+from twisted.web.template import Element, XMLString, renderer
+
 from mock import Mock
 
 def requestMock(path, method="GET", host="localhost", port=8080, isSecure=False):
@@ -44,20 +46,34 @@ def _render(resource, request):
         raise ValueError("Unexpected return value: %r" % (result,))
 
 
+class SimpleElement(Element):
+    loader = XMLString("""
+    <h1 xmlns:t="http://twistedmatrix.com/ns/twisted.web.template/0.1" t:render="name" />
+    """)
+
+    def __init__(self, name):
+        self._name = name
+
+    @renderer
+    def name(self, request, tag):
+        return tag(self._name)
+
+
 class SimpleKlein(KleinResource):
     def __init__(self):
-        self.requestsReceived = []
         self.deferred = None
 
     @expose("/")
     def index(self, request):
-        self.requestsReceived.append(request)
         return 'ok'
 
     @expose("/deferred")
     def deferred(self, request):
-        self.requestsReceived.append(request)
         return self.deferred
+
+    @expose("/element/<string:name>")
+    def element(self, request, name):
+        return SimpleElement(name)
 
 
 class KleinResourceTests(unittest.TestCase):
@@ -70,7 +86,6 @@ class KleinResourceTests(unittest.TestCase):
 
         def _cb(result):
             request.write.assert_called_with('ok')
-            self.assertEquals(kr.requestsReceived, [request])
 
         d.addCallback(_cb)
 
@@ -86,9 +101,21 @@ class KleinResourceTests(unittest.TestCase):
 
         def _cb(result):
             request.write.assert_called_with('ok')
-            self.assertEquals(kr.requestsReceived, [request])
 
         d.addCallback(_cb)
         kr.deferred.callback('ok')
+
+        return d
+
+    def test_elementRendering(self):
+        kr = SimpleKlein()
+        request = requestMock("/element/foo")
+
+        d = _render(kr, request)
+
+        def _cb(result):
+            request.write.assert_called_with("<h1>foo</h1>")
+
+        d.addCallback(_cb)
 
         return d

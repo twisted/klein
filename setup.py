11712cfdfff7a02dfc9199de18fb63995e9a8983
David Reid <dreid@dreid.org>
2012-02-11 20:10:07 -0800

    Setuptools w/ dependencies

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="klein",
     version="0.0.1",
     description="werkzeug + twisted.web",
-    packages=["klein"]
+    packages=["klein"],
+    install_requires=["Twisted", "werkzeug"]
 )

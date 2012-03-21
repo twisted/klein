75d851d21be482cdeb223b2f7cd3444b59ab260d
David Reid <dreid@dreid.org>
2012-03-21 15:09:13 -0700

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="klein",
-    version="0.0.1",
+    version="0.1.0",
     description="werkzeug + twisted.web",
     packages=["klein"],
     install_requires=["Twisted", "werkzeug", "mock"]

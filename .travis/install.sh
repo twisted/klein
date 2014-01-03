a05fb20937dfaeaa75a99d4f7b59bf9bbc9f2b27
HawkOwl <hawkowl@atleastfornow.net>
2014-01-03 12:50:38 +0800

    fixing install script + 13.0

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,14 +6,10 @@
 set -e
 set -x
 
-if [[ "${TOX_ENV}" == "pypy" ]]; then
-sudo add-apt-repository -y ppa:pypy/ppa
-fi
-
-sudo apt-get -y update
-
 if [[ "${TOX_ENV}" == "pypy*" ]]; then
-sudo apt-get install -y pypy
+    sudo add-apt-repository -y ppa:pypy/ppa
+    sudo apt-get -y update
+    sudo apt-get install -y pypy
 
     # This is required because we need to get rid of the Travis installed PyPy
     # or it'll take precedence over the PPA installed one.

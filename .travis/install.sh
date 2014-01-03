24fce5822e2e0267a6a27f6120a565e4b2bafa62
HawkOwl <hawkowl@atleastfornow.net>
2014-01-03 12:58:50 +0800

    need pypy-dev to run twisted

--- .travis/install.sh
+++ .travis/install.sh
@@ -9,7 +9,7 @@ set -x
 if [[ "${TOX_ENV}" == "pypy"* ]]; then
     sudo add-apt-repository -y ppa:pypy/ppa
     sudo apt-get -y update
-    sudo apt-get install -y pypy
+    sudo apt-get install -y pypy pypy-dev
 
     # This is required because we need to get rid of the Travis installed PyPy
     # or it'll take precedence over the PPA installed one.

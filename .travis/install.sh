600a5d81063fdc168030474d191f762ce1c3b84a
HawkOwl <hawkowl@atleastfornow.net>
2014-01-03 12:53:45 +0800

    one day, hawkowl will learn how to use bash

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,7 +6,7 @@
 set -e
 set -x
 
-if [[ "${TOX_ENV}" == "pypy*" ]]; then
+if [[ "${TOX_ENV}" == "pypy"* ]]; then
     sudo add-apt-repository -y ppa:pypy/ppa
     sudo apt-get -y update
     sudo apt-get install -y pypy

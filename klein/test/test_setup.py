96fa257bbf0f17e5045dcccc0b7fe71a57bd9edf
Hynek Schlawack <hs@ox.cx>
2014-01-26 13:06:31 +0100

    Add unit tests for setup.py helper functions

---
+++ klein/test/test_setup.py
@@ -0,0 +1,39 @@
+import os
+from mock import Mock
+from twisted.trial import unittest
+
+import setup
+
+
+class SetupTestCase(unittest.TestCase):
+    """
+    Tests for helper functions whithin setup.py.
+    """
+    def test_read(self):
+        """
+        Reads a file and returns unicode.
+        """
+        contents = 'foo'
+        path = os.path.abspath(self.mktemp())
+        with open(path, 'w') as f:
+            f.write(contents)
+
+        rv = setup.read(path)
+        self.assertEqual(contents, rv)
+        self.assertIsInstance(rv, unicode)
+
+    def test_findVersion(self):
+        """
+        Finds the version that follows our scheme.
+        """
+        self.patch(setup, 'read', Mock(return_value='''
+from something import something_else
+
+__author__ = "Foo"
+__version__ = "4.2.0"
+__license__ = 'MIT'
+'''))
+        self.assertEqual(
+            u"4.2.0",
+            setup.find_version('paths', 'do', 'not', 'matter')
+        )
--- setup.py
+++ setup.py
@@ -28,32 +28,33 @@ def find_version(*file_paths):
     raise RuntimeError("Unable to find version string.")
 
 
-setup(
-    classifiers=[
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ],
-    description="werkzeug + twisted.web",
-    long_description=read('README.rst'),
-    install_requires=[
-        "Twisted>=12.1",
-        "werkzeug"
-    ],
-    keywords="twisted flask werkzeug web",
-    license="MIT",
-    name="klein",
-    packages=["klein"],
-    url="https://github.com/twisted/klein",
-    version=find_version('klein', '__init__.py'),
-    maintainer='David Reid',
-    maintainer_email='dreid@dreid.org',
-)
+if __name__ == "__main__":
+    setup(
+        classifiers=[
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: Implementation :: CPython',
+            'Programming Language :: Python :: Implementation :: PyPy',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+            'Topic :: Software Development :: Libraries :: Python Modules'
+        ],
+        description="werkzeug + twisted.web",
+        long_description=read('README.rst'),
+        install_requires=[
+            "Twisted>=12.1",
+            "werkzeug"
+        ],
+        keywords="twisted flask werkzeug web",
+        license="MIT",
+        name="klein",
+        packages=["klein"],
+        url="https://github.com/twisted/klein",
+        version=find_version('klein', '__init__.py'),
+        maintainer='David Reid',
+        maintainer_email='dreid@dreid.org',
+    )

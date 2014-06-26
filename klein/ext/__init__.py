603ed9e3e5761c5f299caae0f6e602e794b9655b
Ilja Orlovs <ilja.orlovs@cranfield.ac.uk>
2014-06-26 16:06:40 +0100

    Converted tabs to spaces

--- klein/ext/__init__.py
+++ klein/ext/__init__.py
@@ -1,21 +1,25 @@
+# -*- test-case-name: klein.test.test_ext -*-
+
+"""Dynamic extension grabbing module."""
+
 def load_extensions():
-	"""Load extensions.
+    """Load extensions.
 
-	Returns dictionary of extension modules.
-	Keys of the dictionary are the named under which given modules should be made available in this package.
-	"""
-	import pkgutil
+    Returns dictionary of extension modules.
+    Keys of the dictionary are the named under which given modules should be made available in this package.
+    """
+    import pkgutil
 
-	prefix = "klein_"
-	rv = {}
+    prefix = "klein_"
+    rv = {}
 
-	for (loader, name, ispkg) in pkgutil.iter_modules():
-		if name.startswith(prefix):
-			local_name = name[len(prefix):]
-			module = __import__(name)
-			rv[local_name] = module
+    for (loader, name, ispkg) in pkgutil.iter_modules():
+        if name.startswith(prefix):
+            local_name = name[len(prefix):]
+            module = __import__(name)
+            rv[local_name] = module
 
-	return rv
+    return rv
 
 exts = load_extensions()
 globals().update(exts)

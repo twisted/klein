70f14eb09962844b77c06bba2424eaba5b7b81b9
Glyph <glyph@twistedmatrix.com>
2016-07-22 21:20:51 -0700

    first, very basic, test case

---
+++ src/klein/test/test_plating.py
@@ -0,0 +1,55 @@
+
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals
+)
+
+from klein.plating import Plating
+from twisted.web.template import tags, slot
+
+from klein.test.test_resource import requestMock, _render
+from klein.test.util import TestCase
+from klein import Klein
+
+plating = Plating(
+    dict(
+        title="JUST A TITLE",
+        content="NEVER MIND THE CONTENT",
+    ),
+    tags=tags.html(
+        tags.head(tags.title(slot("title"))),
+        tags.body(
+            tags.h1(slot("title")),
+            tags.div(slot("content"),
+                     Class="content")
+        )
+    ),
+)
+
+
+class PlatingTests(TestCase):
+    """
+    Tests for L{Plating}.
+    """
+
+    def setUp(self):
+        """
+        Create an app and a resource wrapping that app for this test.
+        """
+        self.app = Klein()
+        self.kr = self.app.resource()
+
+
+    def test_template(self):
+        """
+        Rendering a L{Plating.content} decorated route results in stuff.
+        """
+
+        @self.app.route("/")
+        @plating.content(tags.span(slot("ok")))
+        def plateMe(request):
+            return {"ok": "test-data-present"}
+
+        request = requestMock(b"/")
+        d = _render(self.kr, request)
+        self.successResultOf(d)
+        self.assertIn(b'test-data-present', request.getWrittenData())

6f08d72c1224e2408986d113cb111b1caebaf0a9
Glyph <glyph@twistedmatrix.com>
2016-07-23 15:00:06 -0700

    do content negotiation via query param
    proper Accept-header based version should happen too, but this version
    is simpler and we want it for browser-based inspection anyway

--- src/klein/plating.py
+++ src/klein/plating.py
@@ -1,6 +1,7 @@
 # -*- test-case-name: klein.test.test_plating -*-
 
 from functools import wraps
+from json import dumps as json_serialize
 
 from twisted.web.template import TagLoader, Element
 
@@ -24,29 +25,34 @@ class Plating(object):
         self._defaults = defaults
         self._loader = TagLoader(tags)
 
-    def routed(self, routing, content_template, *args):
+    def routed(self, routing, content_template):
         """
         
         """
         @wraps(routing)
         def mydecorator(method):
-            return routing(self.content(content_template, *args)(method))
+            loader = TagLoader(content_template)
+            @routing
+            @wraps(method)
+            def mymethod(request, *args, **kw):
+                data = method(request, *args, **kw)
+                if _should_return_json(request):
+                    json_data = self._defaults.copy()
+                    del json_data["content"]
+                    json_data.update(data)
+                    return json_serialize(json_data)
+                else:
+                    return self._elementify(loader, data)
+            return method
         return mydecorator
 
-    def content(self, content_template):
+    def _elementify(self, loader, toFillWith):
         """
         
         """
-        loader = TagLoader(content_template)
-        def decorate(thunk):
-            @wraps(thunk)
-            def decorator(*a, **k):
-                slotData = self._defaults.copy()
-                toFillWith = thunk(*a, **k)
-                slotData.update(toFillWith)
-                slotData.update(content=Element(loader=loader))
-                [loaded] = self._loader.load()
-                loaded = loaded.clone()
-                return Element(loader=TagLoader(loaded.fillSlots(**slotData)))
-            return decorator
-        return decorate
+        slotData = self._defaults.copy()
+        slotData.update(toFillWith)
+        slotData.update(content=Element(loader=loader))
+        [loaded] = self._loader.load()
+        loaded = loaded.clone()
+        return Element(loader=TagLoader(loaded.fillSlots(**slotData)))

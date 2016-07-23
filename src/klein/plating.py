3ccb88b6f24669d2e1e6600be4639c12cd9751fa
Glyph <glyph@twistedmatrix.com>
2016-07-23 14:27:32 -0700

    simplest thing to make the test pass

--- src/klein/plating.py
+++ src/klein/plating.py
@@ -1,24 +1,43 @@
 # -*- test-case-name: klein.test.test_plating -*-
 
 from functools import wraps
-import json
+
+from twisted.web.template import TagLoader, Element
 
 class Plating(object):
     """
     
     """
-    def __init__(self, *a, **k):
+    def __init__(self, defaults, tags):
         """
         
         """
+        self._defaults = defaults
+        self._loader = TagLoader(tags)
 
-    def content(self, *args):
+    def routed(self, routing, content_template, *args):
         """
         
         """
-        def decorator(arg):
-            @wraps(arg)
-            def wrapper(*args, **kwargs):
-                return json.dumps(arg(*args, **kwargs))
-            return wrapper
-        return decorator
+        @wraps(routing)
+        def mydecorator(method):
+            return routing(self.content(content_template, *args)(method))
+        return mydecorator
+
+    def content(self, content_template):
+        """
+        
+        """
+        loader = TagLoader(content_template)
+        def decorate(thunk):
+            @wraps(thunk)
+            def decorator(*a, **k):
+                slotData = self._defaults.copy()
+                toFillWith = thunk(*a, **k)
+                slotData.update(toFillWith)
+                slotData.update(content=Element(loader=loader))
+                [loaded] = self._loader.load()
+                loaded = loaded.clone()
+                return Element(loader=TagLoader(loaded.fillSlots(**slotData)))
+            return decorator
+        return decorate

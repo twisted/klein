abb25df8485171a2fcc2d6a6ad8fcf5fde04b2db
Glyph <glyph@twistedmatrix.com>
2016-08-06 22:37:59 -0700

    just a lazy saturday
    forms need sessions for CSRF protection, so ... what about that?

---
+++ src/klein/_session.py
@@ -0,0 +1,85 @@
+
+import attr
+
+from zope.interface import implementer
+from klein.interfaces import ISessionProcurer, SessionMechanism, NoSuchSession
+from twisted.internet.defer import inlineCallbacks, returnValue
+
+@implementer(ISessionProcurer)
+@attr.s
+class SessionProcurer(object):
+    """
+    Session procurer.
+    """
+    _store = attr.ib()
+    _request = attr.ib()
+
+    _max_age = attr.ib(default=3600)
+    _secure_cookie = attr.ib(default=b"Klein-Secure-Session")
+    _insecure_cookie = attr.ib(default=b"Klein-INSECURE-Session")
+    _cookie_domain = attr.ib(default=None)
+    _cookie_path = attr.ib(default=b"/")
+
+    _secure_auth_header = attr.ib(default=b"X-Auth-Token")
+    _insecure_auth_header = attr.ib(default=b"X-Insecure-Session")
+
+
+    @inlineCallbacks
+    def procure_session(self, force_insecure=False):
+        """
+        Retrieve a session based on this request.
+        """
+        if self._request.isSecure():
+            if force_insecure:
+                auth_header = self._insecure_auth_header
+                cookie_name = self._insecure_cookie
+                sent_securely = False
+            else:
+                auth_header = self._secure_auth_header
+                cookie_name = self._secure_cookie
+                sent_securely = True
+        else:
+            # Have we inadvertently disclosed a secure token over an insecure
+            # transport, for example, due to a buggy client?
+            all_possible_sent_tokens = (
+                sum([self._request.requestHeaders.getRawHeaders(header)
+                     for header in [self._secure_auth_header,
+                                    self._insecure_auth_header]]) +
+                [it for it in [self._request.getCookie(cookie)
+                               for cookie in [self._secure_cookie,
+                                              self._insecure_cookie]] if it]
+            )
+            # Does it seem like this check is expensive? It sure is! Don't want
+            # to do it? Turn on your dang HTTPS!
+            yield self._store.sent_insecurely(all_possible_sent_tokens)
+            auth_header = self._insecure_auth_header
+            cookie_name = self._insecure_cookie
+            sent_securely = False
+            # Fun future feature: honeypot that does this over HTTPS, but sets
+            # isSecure() to return false because it serves up a cert for the
+            # wrong hostname or an invalid cert, to keep API clients honest
+            # about chain validation.
+        mechanism = SessionMechanism.Cookie
+        session_id = self._request.getHeader(auth_header)
+        if session_id is not None:
+            mechanism = SessionMechanism.Header
+        else:
+            session_id = self._request.getCookie(cookie_name)
+        if session_id is not None:
+            try:
+                session = yield self._store.load_session(
+                    session_id, sent_securely, mechanism
+                )
+            except NoSuchSession:
+                if mechanism == SessionMechanism.Header:
+                    raise
+                session_id = None
+        if session_id is None:
+            session = yield self._store.new_session(sent_securely, mechanism)
+        if session_id != session.identifier:
+            self._request.addCookie(
+                cookie_name, session.identifier, max_age=self._max_age,
+                domain=self._cookie_domain, path=self._cookie_path,
+                secure=sent_securely, httpOnly=True,
+            )
+        returnValue(session)
--- src/klein/interfaces.py
+++ src/klein/interfaces.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import, division
 
 from zope.interface import Interface, Attribute
 
+from twisted.python.constants import Names, NamedConstant
+
 
 class IKleinRequest(Interface):
     branch_segments = Attribute("Segments consumed by a branch route.")
@@ -11,3 +13,149 @@ class IKleinRequest(Interface):
         """
         L{werkzeug.routing.MapAdapter.build}
         """
+
+
+
+class NoSuchSession(Exception):
+    """
+    No such session could be found.
+    """
+
+
+
+class ISessionStore(Interface):
+    """
+    Backing storage for sessions.
+    """
+
+    def new_session(is_confidential, authenticated_by):
+        """
+        Create a new L{ISession}.
+
+        @return: a new session with a new identifier.
+        @rtype: L{Deferred} firing with L{ISession}.
+        """
+
+
+    def load_session(identifier, is_confidential, authenticated_by):
+        """
+        Load a session given the given identifier and security properties.
+
+        As an optimization for session stores where the back-end can generate
+        session identifiers when the presented one is not found in the same
+        round-trip to a data store, this method may return a L{Session} object
+        with an C{identifier} attribute that does not match C{identifier}.
+        However, please keep in mind when implementing L{ISessionStore} that
+        this behavior is only necessary for requests where C{authenticated_by}
+        is L{SessionMechanism.Cookie}; an unauthenticated
+        L{SessionMechanism.Header} session is from an API client and its
+        session should be valid already.
+
+        @return: an existing session with the given identifier.
+        @rtype: L{Deferred} firing with L{ISession} or failing with
+            L{NoSuchSession}.
+        """
+
+
+    def sent_insecurely(identifiers):
+        """
+        The transport layer has detected that the given identifiers have been
+        sent over an unauthenticated transport.
+        """
+
+
+
+class ISessionProcurer(Interface):
+    """
+    An L{ISessionProcurer} binds an L{ISessionStore} to a specific L{request
+    <twisted.web.server.Request>} and can procure a session from that request.
+    """
+
+    def procure_session(force_insecure):
+        """
+        Retrieve a session using whatever technique is necessary.
+
+        If the request already identifies an existing session in the store,
+        retrieve it.  If not, create a new session and retrieve that.
+
+        @return: a L{Deferred} that fires with an L{ISession} provider.
+        """
+
+
+class SessionMechanism(Names):
+    """
+    Mechanisms which can be used to identify and authenticate a session.
+
+    @cvar Cookie: The Cookie session mechanism involves looking up the session
+        identifier via an HTTP cookie.  Session objects retrieved via this
+        mechanism may be vulnerable to U{CSRF attacks
+        <https://www.owasp.org/index.php/Cross-Site_Request_Forgery_(CSRF)>}
+        and therefore must have CSRF protections applied to them.
+
+    @cvar Header: The Header mechanism retrieves the session identifier via a
+        separate header such as C{"X-Auth-Token"}.  Since a different-origin
+        site in a browser can easily send a form submission including cookies,
+        but I{can't} easily put stuff into other arbitrary headers, this does
+        not require additional protections.
+    """
+
+    Cookie = NamedConstant()
+    Header = NamedConstant()
+
+
+
+class ISession(Interface):
+    """
+    An L{ISession} provider contains an identifier for the session, information
+    about how the session was negotiated with the client software, and
+    """
+
+    identifier = Attribute(
+        """
+        L{bytes} identifying a session.
+
+        This value should be:
+
+            1. I{unique} - no two sessions have the same identifier
+
+            2. I{unpredictable} - no one but the receipient of the session
+               should be able to guess what it is
+
+            3. I{opaque} - it should contain no interesting information
+        """
+    )
+
+    is_confidential = Attribute(
+        """
+        A L{bool} indicating whether this session mechanism transmitted over an
+        encrypted transport, i.e., HTTPS.  If C{True}, this means that this
+        session can be used for sensitive information; otherwise, the
+        information contained in it should be considered to be available to
+        attackers.
+        """
+    )
+
+    authenticated_by = Attribute(
+        """
+        A L{SessionMechanism} indicating what mechanism was used to
+        authenticate this session.
+        """
+    )
+
+    data = Attribute(
+        """
+        A L{Componentized} representing application-specific data loaded for
+        this session.
+
+        @see: L{ISessionStore.load_session_data}
+        """
+    )
+
+    def save():
+        """
+        Save any changed values in this session's L{data} attribute.
+
+        @return: a L{Deferred} that fires with C{None} when the data or saved,
+            or fails with a backing-store-specific exception if it can't be.
+        """
+

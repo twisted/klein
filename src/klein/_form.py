6441b56f8b9d9e5830529828bf7491666b28dd2d
Glyph <glyph@twistedmatrix.com>
2016-08-07 03:11:08 -0700

    actually sketch out where CSRF stuff would go
    fix examples to stuff renderable form into arglist

--- src/klein/_form.py
+++ src/klein/_form.py
@@ -7,18 +7,29 @@ from twisted.internet.defer import inlineCallbacks, returnValue
 from twisted.web.template import tags, MissingRenderMethod
 from twisted.web.iweb import IRenderable
 
-from klein.interfaces import ISessionProcurer
+from klein.interfaces import SessionMechanism
+from klein.app import _call
+
+class CrossSiteRequestForgery(Exception):
+    """
+    Cross site request forgery detected.  Request aborted.
+    """
+
+CSRF_PROTECTION = "__csrf_protection__"
 
 @attr.s
 class Field(object):
     """
     
     """
+
     coercer = attr.ib()
     form_input_type = attr.ib()
     python_argument_name = attr.ib(default=None)
     form_field_name = attr.ib(default=None)
     form_label = attr.ib(default=None)
+    no_label = attr.ib(default=False)
+    value = attr.ib(default=u"")
 
     def maybe_named(self, name):
         """
@@ -28,16 +39,22 @@ class Field(object):
             self.python_argument_name = name
         if self.form_field_name is None:
             self.form_field_name = name
-        if self.form_label is None:
+        if self.form_label is None and not self.no_label:
             self.form_label = name.capitalize()
+        return self
+
 
     def as_tags(self):
         """
         
         """
-        return tags.label(self.form_label, ": ",
-                          tags.input(type=self.form_input_type,
-                          name=self.form_field_name))
+        input_tag = tags.input(type=self.form_input_type,
+                               name=self.form_field_name,
+                               value=self.value)
+        if self.form_label:
+            return tags.label(self.form_label, ": ", input_tag)
+        else:
+            return input_tag
 
 
     def extract_from_request(self, request):
@@ -55,9 +72,10 @@ class FormRenderer(object):
     
     """
     form = attr.ib()
+    procurer = attr.ib()
     action = attr.ib()
-    enctype = attr.ib()
     method = attr.ib()
+    enctype = attr.ib()
     encoding = attr.ib(default="utf-8")
 
     def lookupRenderMethod(self, name):
@@ -72,13 +90,14 @@ class FormRenderer(object):
         """
         
         """
-        access = ISessionProcurer(request, None)
-        if access is None:
-            raise ValueError("useful message about how to configure session")
-        session = yield access.procure_session()
-        fields = list(self.form.all_fields()) + [Form.hidden(session.uid)]
+        session = yield self.procurer.procure_session()
+        fields = list(self.form.all_fields()) + [
+            Form.hidden(CSRF_PROTECTION,
+                        session.identifier)
+        ]
         field_tags = list(field.as_tags() for field in fields)
         returnValue(field_tags)
+
 
 
 @attr.s(init=False)
@@ -91,13 +110,14 @@ class Form(object):
     form_data = b'multipart/form-data'
     url_encoded = b'application/x-www-form-urlencoded'
 
-    def __init__(self, fields):
+    def __init__(self, fields, procurer_from_request):
         """
         
         """
         for name, field in fields.items():
             field.maybe_named(name)
         self.fields = fields
+        self.procurer_from_request = procurer_from_request
 
 
     def handler(self, route):
@@ -105,22 +125,44 @@ class Form(object):
         
         """
         def decorator(function):
-            def decorated(request, *args, **kw):
+            @route
+            @inlineCallbacks
+            def decorated(instance, request, *args, **kw):
                 print("handling", request.args)
+                procurer = _call(self.procurer_from_request,
+                                 instance, request)
+                session = yield procurer.procure_session()
+                if session.mechanism == SessionMechanism.Cookie:
+                    token = request.args.get(CSRF_PROTECTION)
+                    if token != session.identifier:
+                        raise CrossSiteRequestForgery()
                 for field in self.fields.values():
                     kw[field.python_argument_name] = (
                         field.extract_from_request(request))
-                return function(request, *args, **kw)
-            route(decorated)
+                returnValue(_call(function, instance, request, *args, **kw))
+            decorated.__klein_bound__ = True
             return function
         return decorator
 
 
-    def renderer(self, action, method="POST", enctype=form_data):
+    def renderer(self, route, action, method="POST", enctype=form_data,
+                 argument="form"):
         """
         
         """
-        return FormRenderer(self, action, method, enctype, "utf-8")
+        def decorator(function):
+            @route
+            @inlineCallbacks
+            def decorated(instance, request, *args, **kw):
+                procurer = yield self.procurer_from_request(request)
+                form = FormRenderer(self, procurer, action, method, enctype)
+                kw[argument] = form
+                result = yield _call(function, request, *args, **kw)
+                returnValue(result)
+            decorated.__klein_bound__ = True
+            return function
+        return decorator
+
 
     def all_fields(self):
         """
@@ -151,4 +193,15 @@ class Form(object):
         """
         return Field(coercer=lambda x: unicode(x, "utf-8"),
                      form_input_type="text")
+
+
+    @staticmethod
+    def hidden(name, value):
+        """
+        
+        """
+        return Field(coercer=lambda x: unicode(x, "utf-8"),
+                     form_input_type="hidden",
+                     no_label=True,
+                     value=value).maybe_named(name)
 

0404c3235ec52c4b80c2d39ed8d80ae17b61bfa5
Moshe Zadka <zadka.moshe@gmail.com>
2017-03-03 20:54:27 -0800

    Update to release

--- src/klein/_version.py
+++ src/klein/_version.py
@@ -7,5 +7,5 @@ Provides Klein version information.
 
 from incremental import Version
 
-__version__ = Version('Klein', 17, 2, 0, release_candidate=1)
+__version__ = Version('Klein', 17, 2, 0)
 __all__ = ["__version__"]

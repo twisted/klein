7d506561d9e5f7de0fe6db3cdc3a61e814a7e971
Wilfredo Sánchez <wsanchez@wsanchez.net>
2017-05-23 17:56:02 -0700

    Add doc strings.

--- src/klein/test/_strategies.py
+++ src/klein/test/_strategies.py
@@ -20,9 +20,17 @@ __all__ = ()
 
 
 
-def is_idna_compatible(label, max_length):
+def _is_idna_compatible(text, max_length):
+    """
+    Determine whether some text contains only characters that can be encoded
+    as IDNA, and that the encoded text is less than the given maximum length.
+
+    @param text: The text to check.
+
+    @param max_length: The maximum allowed length for the encoded text.
+    """
     try:
-        idna = label.encode("idna")
+        idna = text.encode("idna")
     except ValueError:
         return False
 
@@ -30,7 +38,12 @@ def is_idna_compatible(label, max_length):
 
 
 @composite
-def port_numbers(draw, allow_zero):
+def port_numbers(draw, allow_zero=False):
+    """
+    A strategy which generates port numbers.
+
+    @param allow_zero: Whether to allow port C{0} as a possible value.
+    """
     if allow_zero:
         min_value = 0
     else:
@@ -41,6 +54,12 @@ def port_numbers(draw, allow_zero):
 
 @composite
 def hostname_labels(draw, allow_idn=True):
+    """
+    A strategy which generates host name labels.
+
+    @param allow_idn: Whether to allow non-ASCII characters as allowed by
+        internationalized domain names (IDNs).
+    """
     alphabet = unicode(ascii_letters + digits + u"-")
     if allow_idn:
         # FIXME: get a more complete character set
@@ -51,19 +70,28 @@ def hostname_labels(draw, allow_idn=True):
     label = draw(text(min_size=1, max_size=63, alphabet=alphabet))
 
     if allow_idn:
-        assume(is_idna_compatible(label, 63))
+        assume(_is_idna_compatible(label, 63))
 
     return label
 
 
 @composite
 def hostnames(draw, allow_leading_digit=True, allow_idn=True):
+    """
+    A strategy which generates host names.
+
+    @param allow_leading_digit: Whether to allow a leading digit in host names;
+        they were not allowed prior to RFC 1123.
+
+    @param allow_idn: Whether to allow non-ASCII characters as allowed by
+        internationalized domain names (IDNs).
+    """
     labels = draw(lists(hostname_labels(allow_idn=allow_idn), min_size=1))
 
     name = u".".join(labels)
 
     if allow_idn:
-        assume(is_idna_compatible(name, 252))
+        assume(_is_idna_compatible(name, 252))
     else:
         assume(len(name) <= 252)
 
@@ -72,6 +100,11 @@ def hostnames(draw, allow_leading_digit=True, allow_idn=True):
 
 @composite
 def http_urls(draw):
+    """
+    A strategy which generates (human-friendly, unicode) IRI-form L{URL}s.
+    Call the C{asURI} method on each URL to get a (network-friendly, ASCII)
+    URI.
+    """
     host = draw(hostnames())
 
     port = draw(port_numbers(allow_zero=True))

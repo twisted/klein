59ef2e2ed2c6387f9bf7fed1f8272f5e94f7011a
Wilfredo Sánchez <wsanchez@wsanchez.net>
2017-05-31 10:52:02 -0700

    Add mypy, ignore E305

--- src/klein/test/py3_test_resource.py
+++ src/klein/test/py3_test_resource.py
@@ -7,6 +7,7 @@ from klein.test.util import TestCase
 from .test_resource import LeafResource, _render, requestMock
 
 
+
 class PY3KleinResourceTests(TestCase):
 
     def assertFired(self, deferred, result=None):

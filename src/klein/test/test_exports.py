d6a0065867ac41297c7957897a4c96cfc4c4a8e0
Wilfredo Sánchez <wsanchez@wsanchez.net>
2017-12-13 17:46:37 -0800

    Add todo tests instead of commented out tests

--- src/klein/test/test_exports.py
+++ src/klein/test/test_exports.py
@@ -22,12 +22,24 @@ class PublicSymbolsTestCase(unittest.TestCase):
 
         self.assertIdentical(k.Klein, a.Klein)
         self.assertIdentical(k.handle_errors, a.handle_errors)
-        # Blocked on https://github.com/twisted/klein/issues/224
-        # self.assertIdentical(k.resource, a.resource)
         self.assertIdentical(k.route, a.route)
         self.assertIdentical(k.run, a.run)
 
         self.assertIdentical(k.Plating, p.Plating)
+
+
+    def test_klein_resource(self):
+        # type: () -> None
+        """
+        Test export of C{resource} from L{klein}.
+        (Move this into C{test_klein} after #224 is fixed.)
+        """
+        import klein as k
+        import klein._app as a
+
+        self.assertIdentical(k.resource, a.resource)
+
+    test_klein_resource.todo = "See #224"
 
 
     def test_app(self):
@@ -56,14 +68,15 @@ class PublicSymbolsTestCase(unittest.TestCase):
         self.assertIdentical(i.IKleinRequest, _i.IKleinRequest)
 
 
-    # Blocked on https://github.com/twisted/klein/issues/224
-    # def test_resource(self):
-    #     # type: () -> None
-    #     """
-    #     Test exports from L{klein.resource}.
-    #     """
-    #     import klein.resource as r
-    #     import klein._resource as _r
+    def test_resource(self):
+        # type: () -> None
+        """
+        Test exports from L{klein.resource}.
+        """
+        import klein.resource as r
+        import klein._resource as _r
 
-    #     self.assertIdentical(r.KleinResource, _r.KleinResource)
-    #     self.assertIdentical(r.ensure_utf8_bytes, _r.ensure_utf8_bytes)
+        self.assertIdentical(r.KleinResource, _r.KleinResource)
+        self.assertIdentical(r.ensure_utf8_bytes, _r.ensure_utf8_bytes)
+
+    test_resource.todo = "See #224"

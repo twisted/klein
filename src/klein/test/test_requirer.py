aa79db45e9bc660bd1aab8d5217cc9a658d2f39e
Glyph <glyph@twistedmatrix.com>
2018-08-03 23:09:07 -0700

    test badly behaved HTTP client

--- src/klein/test/test_requirer.py
+++ src/klein/test/test_requirer.py
@@ -1,16 +1,32 @@
 
+from typing import TYPE_CHECKING
+
 from treq.testing import StubTreq
 
 from twisted.trial.unittest import SynchronousTestCase
-
-from typing import TYPE_CHECKING
+from twisted.web.http_headers import Headers
 
 from klein import Klein, RequestURL, Requirer
 
 if TYPE_CHECKING:               # pragma: no cover
-    from typing import Text
+    from typing import Text, Iterable, List, Tuple
     from hyperlink import DecodedURL
-    DecodedURL, Text
+    DecodedURL, Text, Iterable, List, Tuple
+
+class BadlyBehavedHeaders(Headers):
+    """
+    Make L{Headers} lie, and refuse to return a Host header from
+    getAllRequestHeaders.
+    """
+
+    def getAllRawHeaders(self):
+        # type: () -> Iterable[Tuple[bytes, List[bytes]]]
+        """
+        Don't return a host header.
+        """
+        for key, values in super(BadlyBehavedHeaders, self).getAllRawHeaders():
+            if key != b'Host':
+                yield (key, values)
 
 
 router = Klein()
@@ -37,9 +53,43 @@ class RequireURLTests(SynchronousTestCase):
         """
         response = self.successResultOf(
             self.successResultOf(StubTreq(router.resource()).get(
-                "https://asdf.com/hello/world"
+                "https://example.com/hello/world"
             )).text()
         )
 
         self.assertEqual(response,
-                         "https://asdf.com/hello/world/hello%2F%20world")
+                         "https://example.com/hello/world/hello%2F%20world")
+
+    def test_requiresURLNonStandardPort(self):
+        # type: () -> None
+        """
+        When RequestURL is specified to a requirer, a DecodedURL object will be
+        passed in to the decorated route.
+        """
+        response = self.successResultOf(
+            self.successResultOf(StubTreq(router.resource()).get(
+                "https://example.com:8443/hello/world"
+            )).text()
+        )
+
+        self.assertEqual(
+            response,
+            "https://example.com:8443/hello/world/hello%2F%20world"
+        )
+
+    def test_requiresURLBadlyBehavedClient(self):
+        # type: () -> None
+        """
+        requiresURL will press on in the face of badly-behaved client code.
+        """
+        response = self.successResultOf(
+            self.successResultOf(StubTreq(router.resource()).get(
+                "https://example.com/hello/world",
+                headers=BadlyBehavedHeaders()
+            )).text()
+        )
+        self.assertEqual(
+            response,
+            # Static values from StubTreq - this should probably be tunable.
+            "https://127.0.0.1:31337/hello/world/hello%2F%20world"
+        )

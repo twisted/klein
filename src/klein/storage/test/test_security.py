7058384c7af0ee4086fdfeac5b9ed41da3ec63fb
Glyph <glyph@twistedmatrix.com>
2019-05-07 00:12:55 -0400

    test stubs

---
+++ src/klein/storage/test/test_security.py
@@ -0,0 +1,12 @@
+
+from klein.storage import security
+from twisted.trial.unittest import TestCase
+
+class SQLTests(TestCase):
+
+    def test_security(self):
+        """
+        Add tests.
+        """
+        security
+
---
+++ src/klein/storage/test/test_sql.py
@@ -0,0 +1,10 @@
+
+from klein.storage import sql
+from twisted.trial.unittest import TestCase
+
+class SQLTests(TestCase):
+    def test_sql(self):
+        """
+        Add tests.
+        """
+        sql

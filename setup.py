42cb6fad020d0a15be223ce07921027f69de897b
Hugo <hugovk@users.noreply.github.com>
2019-06-06 08:55:13 +0300

    Drop support for EOL Python 3.4

--- setup.py
+++ setup.py
@@ -6,14 +6,16 @@ if __name__ == "__main__":
         long_description = f.read()
 
     setup(
+        python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
         classifiers=[
             'Environment :: Web Environment',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: MIT License',
             'Operating System :: OS Independent',
             'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
             'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',

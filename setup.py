9ed8a98162aa47cfa2f7eb4f198945f9f3e0230c
Wilfredo Sánchez <wsanchez@wsanchez.net>
2020-06-08 15:24:14 -0700

    Add setup.py to black's radar.

--- setup.py
+++ setup.py
@@ -2,24 +2,24 @@ from setuptools import setup
 
 if __name__ == "__main__":
 
-    with open('README.rst', 'r') as f:
+    with open("README.rst", "r") as f:
         long_description = f.read()
 
     setup(
         classifiers=[
-            'Environment :: Web Environment',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: MIT License',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: Implementation :: CPython',
-            'Programming Language :: Python :: Implementation :: PyPy',
-            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-            'Topic :: Software Development :: Libraries :: Python Modules'
+            "Environment :: Web Environment",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: Implementation :: CPython",
+            "Programming Language :: Python :: Implementation :: PyPy",
+            "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+            "Topic :: Software Development :: Libraries :: Python Modules",
         ],
         description="werkzeug + twisted.web",
         long_description=long_description,
@@ -42,12 +42,8 @@ if __name__ == "__main__":
         name="klein",
         packages=["klein", "klein.storage", "klein.test"],
         package_dir={"": "src"},
-        package_data=dict(
-            klein=[
-                "test/idna-tables-properties.csv",
-            ],
-        ),
+        package_data=dict(klein=["test/idna-tables-properties.csv",],),
         url="https://github.com/twisted/klein",
-        maintainer='Amber Brown (HawkOwl)',
-        maintainer_email='hawkowl@twistedmatrix.com',
+        maintainer="Amber Brown (HawkOwl)",
+        maintainer_email="hawkowl@twistedmatrix.com",
     )

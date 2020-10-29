c052621550a8ceaf90db60dd571c289cd62cfb01
Wilfredo Sánchez <wsanchez@wsanchez.net>
2020-10-29 14:36:06 -0700

    Remove some unused type aliases

--- src/klein/_form.py
+++ src/klein/_form.py
@@ -29,7 +29,7 @@ from zope.interface import Interface, implementer
 
 from ._app import _call
 from ._decorators import bindable
-from ._typing import DefaultNamedArg, NoReturn
+from ._typing import NoReturn
 from .interfaces import (
     EarlyExit,
     IDependencyInjector,
@@ -451,13 +451,6 @@ def defaultValidationFailureHandler(
 
 
 _requirerFunctionWithForm = Any
-_routeCallable = Any
-_routeDecorator = Callable[
-    [_routeCallable, DefaultNamedArg(Any, "__session__")], _routeCallable
-]
-_validationFailureHandler = Callable[
-    [Optional[object], IRequest, "Form", Dict[str, str]], Element
-]
 
 validationFailureHandlerAttribute = "__kleinFormValidationFailureHandlers__"
 

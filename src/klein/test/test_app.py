7d170c62a3e81b6c2b49332f7d5d78c87dddefb7
Wilfredo Sánchez <wsanchez@wsanchez.net>
2021-01-25 05:15:27 -0800

    Ignore use of ad-hoc attribute for testing

--- src/klein/test/test_app.py
+++ src/klein/test/test_app.py
@@ -304,7 +304,7 @@ class KleinTestCase(unittest.TestCase):
             return add(a * 1000, b * 10000)
 
         self.assertEqual(megaAdd.supersized, True)
-        self.assertEqual(add.supersized, True)
+        self.assertEqual(add.supersized, True)  # type: ignore[attr-defined]
         self.assertIn("supersizer for add", str(megaAdd))
         self.assertEqual(megaAdd(3, 4), 43000)
 

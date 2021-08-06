b208a765890a12b369e32772c56a7b6ad082c66b
Wilfredo Sánchez <wsanchez@wsanchez.net>
2021-08-06 16:30:57 -0700

    Bump version for release to 21.8.0-rc1

--- src/klein/_version.py
+++ src/klein/_version.py
@@ -8,5 +8,5 @@ Provides klein version information.
 from incremental import Version
 
 
-__version__ = Version("klein", 20, 6, 0)
+__version__ = Version("klein", 21, 8, 0, release_candidate=1)
 __all__ = ["__version__"]

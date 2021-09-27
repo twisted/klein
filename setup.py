ff9c7be8df146a4af077e0bd9015d4cec5b26e1a
Wilfredo Sánchez <wsanchez@wsanchez.net>
2021-09-27 13:20:11 -0700

    Use Python 3.10 as default

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: Implementation :: CPython",
             "Programming Language :: Python :: Implementation :: PyPy",
             "Topic :: Internet :: WWW/HTTP :: Dynamic Content",

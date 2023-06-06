324872dfb944f3286e267722f48250fc6991294f
Glyph <code@glyph.im>
2023-06-06 01:04:44 -0700

    comments for schema

--- src/klein/storage/sql/basic_auth_schema.sql
+++ src/klein/storage/sql/basic_auth_schema.sql
@@ -1,4 +1,6 @@
 
+-- `session` identifies individual clients with a particular set of
+-- capabilities.  the `session_id` is the secret held by the client.
 CREATE TABLE session (
   session_id VARCHAR NOT NULL,
   confidential BOOLEAN NOT NULL,
@@ -7,6 +9,8 @@ CREATE TABLE session (
   PRIMARY KEY (session_id)
 );
 
+-- `account` is a user with a name and password.  the password_blob is computed
+-- by the password engine in klein.storage.passwords.
 CREATE TABLE account (
   account_id VARCHAR NOT NULL,
   username VARCHAR NOT NULL,
@@ -16,10 +20,15 @@ CREATE TABLE account (
   UNIQUE (username)
 );
 
+-- `session_account` is a record of which acccount is logged in to which session.
 CREATE TABLE session_account (
   account_id VARCHAR,
   session_id VARCHAR,
   UNIQUE (account_id, session_id),
-  FOREIGN KEY(account_id) REFERENCES account (account_id) ON DELETE CASCADE,
-  FOREIGN KEY(session_id) REFERENCES session (session_id) ON DELETE CASCADE
+  FOREIGN KEY(account_id)
+    REFERENCES account (account_id)
+    ON DELETE CASCADE,
+  FOREIGN KEY(session_id)
+    REFERENCES session (session_id)
+    ON DELETE CASCADE
 );

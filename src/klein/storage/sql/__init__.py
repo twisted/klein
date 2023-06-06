d953f1696929e6d68f1307c5ec370ae95d2af07f
Glyph <code@glyph.im>
2023-06-06 01:05:02 -0700

    make SessionStore public, since it pretty much needs to be

--- src/klein/storage/sql/__init__.py
+++ src/klein/storage/sql/__init__.py
@@ -3,11 +3,17 @@ An implementation of a basic username/password authentication database using
 C{dbxs}.
 """
 
-from ._sql_glue import SQLSessionProcurer, applyBasicSchema, authorizerFor
+from ._sql_glue import (
+    SessionStore,
+    SQLSessionProcurer,
+    applyBasicSchema,
+    authorizerFor,
+)
 
 
 __all__ = [
     "SQLSessionProcurer",
+    "SessionStore",
     "authorizerFor",
     "applyBasicSchema",
 ]

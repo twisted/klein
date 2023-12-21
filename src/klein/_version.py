737c32aaed217131d0773830c917e1269a297ebf
Glyph <code@glyph.im>
2023-12-21 17:12:45 -0500

    start 23.12 release

--- src/klein/_version.py
+++ src/klein/_version.py
@@ -7,5 +7,5 @@ Provides klein version information.
 
 from incremental import Version
 
-__version__ = Version("klein", 23, 5, 0)
+__version__ = Version("klein", 23, 12, 0, release_candidate=1)
 __all__ = ["__version__"]

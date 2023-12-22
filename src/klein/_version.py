1ca5d3b3c1bfb8f74799ccd8d75f2ff1663274b1
Glyph <code@glyph.im>
2023-12-21 23:27:51 -0500

    roll everything back to re-test release script

--- src/klein/_version.py
+++ src/klein/_version.py
@@ -7,5 +7,5 @@ Provides Klein version information.
 
 from incremental import Version
 
-__version__ = Version("Klein", 23, 12, 0, release_candidate=4)
+__version__ = Version("Klein", 23, 11, 0)
 __all__ = ["__version__"]

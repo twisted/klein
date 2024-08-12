5b00a588929113ac285ae069d957ad0891306bac
Glyph <code@glyph.im>
2024-08-11 18:41:18 -0700

    Update version to [Klein, version 24.8.0.rc3]

--- src/klein/_version.py
+++ src/klein/_version.py
@@ -7,5 +7,5 @@ Provides Klein version information.
 
 from incremental import Version
 
-__version__ = Version("Klein", 24, 8, 0, release_candidate=2)
+__version__ = Version("Klein", 24, 8, 0, release_candidate=3)
 __all__ = ["__version__"]

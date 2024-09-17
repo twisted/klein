61249999c6f0d790ed78ebc2efa5efb0bb1a3ebb
Itamar Turner-Trauring <itamar@pythonspeed.com>
2024-09-17 14:56:01 -0400

    Add more supported versions

--- setup.py
+++ setup.py
@@ -17,6 +17,8 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
+            "Programming Language :: Python :: 3.13",
             "Programming Language :: Python :: Implementation :: CPython",
             "Programming Language :: Python :: Implementation :: PyPy",
             "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
